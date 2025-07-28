2d226764975a98067028041e6d74fb1daef3597b
isaac S. Silva <85599926+IsaacSSilva@users.noreply.github.com>
2025-07-28 18:03:57 -0300

    remove: unwanted js

--- src/v1/src/core/matchShortcut.core.js
+++
@@ -1,6 +0,0 @@
-export function filterShortcutsByRange(shortcuts, activeRanges) {
-    return shortcuts.filter((shortcut) => shortcut.range.some((r) => activeRanges.includes(r)));
-}
-export function isComboPressed(combo, pressedKeys) {
-    return combo.every((k) => pressedKeys.has(k));
-}
--- src/v1/src/core/normalizeShortcuts.core.js
+++
@@ -1,6 +0,0 @@
-export function normalizeShortcuts(shortcuts) {
-    return shortcuts.map((s) => ({
-        ...s,
-        range: s.range && s.range.length > 0 ? s.range : ["global"],
-    }));
-}
